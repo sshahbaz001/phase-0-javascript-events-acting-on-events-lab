4d836f73d32eb32888f1677d2b28bb9b6ff432e9
Saba Shahbaz <sabashahbaz001@gmail.com>
2023-06-10 23:46:32 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    console.log(dodger.style.left)
+    if (left < 360) {
+      console.log(dodger.style.left)
+      dodger.style.left = `${left + 1}px.`;
+    }
+    console.log(dodger.style.left)
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  
+  });
+
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
+
+
